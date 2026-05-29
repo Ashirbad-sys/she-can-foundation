1e085dfa92fa083ddd518f277e3e994a80b19fd3
Ashirbad-sys <ashirbadpattanayak05@gmail.com>
2026-05-29 15:43:31 +0530

    Update API URL to Render

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -225,7 +225,7 @@ contactForm.addEventListener("submit", async (e) => {
   };
 
   try {
-  const res  = await fetch("http://localhost:3001/api/contact", {
+  const res = await fetch("https://she-can-foundation-lqxa.onrender.com/api/contact", {
     method:  "POST",
     headers: { "Content-Type": "application/json" },
     body:    JSON.stringify(formData),
